dc618150d8f460c2c45e6a8340f2ff5335be79f0
Sam Burba <sburba@barracuda.com>
2019-04-08 22:23:53 -0400

    Multithread mutation testing execution

--- scripts/run_infection.sh
+++ scripts/run_infection.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-$(dirname $0)/run_in_docker.sh vendor/bin/infection --initial-tests-php-options="-d zend_extension=xdebug.so"
+$(dirname $0)/run_in_docker.sh vendor/bin/infection --initial-tests-php-options="-d zend_extension=xdebug.so" --threads=$(nproc)
