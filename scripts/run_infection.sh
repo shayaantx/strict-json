fd11c7fa7a5aa5ca780df8e0a64792228dc657a9
Sam Burba <sburba@barracuda.com>
2019-04-08 23:43:44 -0400

    Remove mutation false positives, upgrade minimum mutation scores
    We don't use or change the error code anywhere, so it doesn't make sense
    to test for it

--- scripts/run_infection.sh
+++ scripts/run_infection.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 
 $(dirname $0)/run_in_docker.sh vendor/bin/infection \
     --initial-tests-php-options="-d zend_extension=xdebug.so" --threads=$(nproc) \
-    --min-msi=79 \
-    --min-covered-msi=81
+    --min-msi=84 \
+    --min-covered-msi=87
