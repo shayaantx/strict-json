e132610febe7cc077880202f5f48866a6d9cc9f2
Sam Burba <gitlab@samburba.com>
2019-04-09 22:26:07 -0400

    Increase mutation score targets to match new coverage

--- scripts/run_infection.sh
+++ scripts/run_infection.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 
 $(dirname $0)/run_in_docker.sh vendor/bin/infection \
     --initial-tests-php-options="-d zend_extension=xdebug.so" --threads=$(nproc) \
-    --min-msi=84 \
-    --min-covered-msi=87
+    --min-msi=89 \
+    --min-covered-msi=92
