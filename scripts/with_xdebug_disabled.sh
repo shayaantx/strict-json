492d280f4967f31a844bfebf8ad08f4646e384a0
Sam Burba <gitlab@samburba.com>
2019-04-20 19:13:17 -0400

    Support php configurations where xdebug is not the last extension

--- scripts/with_xdebug_disabled.sh
+++ scripts/with_xdebug_disabled.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -eu
 
-xdebug_ini=$(php -i | grep "xdebug.*\.ini" -m 1)
+xdebug_ini=$(php -i | grep -m 1 xdebug | sed 's/,//')
 if [[ -z "${xdebug_ini}" ]]; then
     eval "$@"
     exit $?
 fi
 
-mv "${xdebug_ini}" "${xdebug_ini}.disabled"
+if [[ $(id -u) = 0 ]]; then
+    mv_cmd="mv"
+else
+    mv_cmd="sudo mv"
+fi
+
+${mv_cmd} "${xdebug_ini}" "${xdebug_ini}.disabled"
 set +e
 eval "$@"
 exit_code=$?
 set -e
-mv "${xdebug_ini}.disabled" "${xdebug_ini}"
+${mv_cmd} "${xdebug_ini}.disabled" "${xdebug_ini}"
 exit ${exit_code}
