bfff6238f531c2926c496dd8fbec135373fa3ee6
Sam Burba <gitlab@samburba.com>
2019-04-21 12:41:16 -0400

    Benchmark handwritten alternative to StrictJson to compare performance

--- benchmark/LargeListBench.php
+++ benchmark/LargeListBench.php
@@ -1,5 +1,6 @@
 <?php declare(strict_types=1);
 
+use Burba\StrictJson\Fixtures\Docs\Address;
 use Burba\StrictJson\Fixtures\Docs\Event;
 use Burba\StrictJson\Fixtures\Docs\User;
 use Burba\StrictJson\JsonFormatException;
@@ -31,5 +32,28 @@ class LargeListBench
     public function benchLargeList()
     {
         $this->mapper->mapToArrayOf($this->json, User::class);
+    }
+
+    public function benchLargeListHandwritten()
+    {
+        $decoded_json = json_decode($this->json, true);
+        if (!is_array($decoded_json)) {
+            throw new RuntimeException('Invalid JSON');
+        }
+
+        $users = [];
+        foreach ($decoded_json as $json_object) {
+            $name = $json_object['name'] ?? null;
+            $age = $json_object['age'] ?? null;
+            $street = $json_object['address']['street'] ?? null;
+            $zip_code = $json_object['address']['zip_code'] ?? null;
+
+            if (!is_string($name) || !is_int($age) || !is_string($street) || !is_string($zip_code)) {
+                throw new RuntimeException('Invalid JSON');
+            }
+
+            $address = new Address($street, $zip_code);
+            $users[] = new User($name, $age, $address);
+        }
     }
 }
