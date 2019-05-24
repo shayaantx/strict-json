bc1a5166b1f9d81a099a28d6797dc70ba000eb83
Sam Burba <gitlab@samburba.com>
2019-05-23 20:49:52 -0400

    Remove pointless double negative

--- scripts/deploy_docs.sh
+++ scripts/deploy_docs.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 branch=${CIRCLE_BRANCH:-$(git symbolic-ref --short -q HEAD)}
 repository=https://${GITHUB_TOKEN}@github.com/sburba/strict-json
 
-if [[ ! -z ${CIRCLE_PULL_REQUEST:-} ]]; then
+if [[ -n ${CIRCLE_PULL_REQUEST:-} ]]; then
     echo "Skipping docs deploy, PR branch"
     exit 0
 fi
@@ -14,7 +14,7 @@ if [[ ! ${branch} == 'master' ]]; then
     exit 0
 fi
 
-if [[ ! -z ${CI:-} ]]; then
+if [[ -n ${CI:-} ]]; then
     git config --global user.name "CI AutoDeploy"
     git config --global user.email "autodeploy@samburba.com"
 fi
